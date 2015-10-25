6d47fed5ddf56329ddbc765abb0c9661e36b2987
Alex <alex@rechenkobold.datenklo>
2015-10-25 16:48:38 +0100

    addec copyright notice

--- src/main.c
+++ src/main.c
@@ -1,29 +1,26 @@
-
 /*
- * This file is part of the libopencm3 project.
+ * This file is part of ROCKCLOCK firmware.
  *
- * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
- * Copyright (C) 2011 Stephen Caudle <scaudle@doceme.com>
- *
- * This library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * Copyright (c) 2015 Alexander Kühn <prj@alexkuehn.de>
+ *     
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
+ *     
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * GNU General Public License for more details.
+ *       
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "hal/hal_if.h"
 #include "os/os_if.h"
 #include "os/scheduler_if.h"
 #include "app/app_if.h"
-
 
 
 int main(void)
