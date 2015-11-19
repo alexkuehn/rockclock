6004b9d206b29ad28cdb06e6e81170608793e354
Alexander Kühn <prj@alexkuehn.de>
2015-11-19 20:08:40 +0100

    added i2c infrastructure

--- src/hal/i2c.c
+++ src/hal/i2c.c
@@ -2,17 +2,17 @@
  * This file is part of ROCKCLOCK firmware.
  *
  * Copyright (c) 2015 Alexander Kühn <prj@alexkuehn.de>
- *     
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- *     
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *       
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -20,27 +20,17 @@
 /* external standard includes */
 
 /* external includes */
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
 
 /* project includes */
-#include "cpu.h"
-#include "io.h"
-#include "timer.h"
-#include "usart.h"
-#include "ws2812.h"
-#include "dcf_if.h"
-#include "i2c.h"
 
 /* component includes */
+#include "i2c_config.h"
+#include "../hal/i2c_if.h"
 
 
-void hal_init( void )
+void i2c_init( void )
 {
-	/* initialize the HAL subcomponents */
-	cpu_init();
-	io_init();
-	usart_init();
-	timer_init();
-	dcf_init();
-	ws2812_init();
-	i2c_init();
 }
+
--- src/hal/i2c.h
+++ src/hal/i2c.h
@@ -2,45 +2,35 @@
  * This file is part of ROCKCLOCK firmware.
  *
  * Copyright (c) 2015 Alexander Kühn <prj@alexkuehn.de>
- *     
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- *     
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *       
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
+#ifndef SRC_HAL_I2C_H_
+#define SRC_HAL_I2C_H_
 
 /* external standard includes */
 
 /* external includes */
 
 /* project includes */
-#include "cpu.h"
-#include "io.h"
-#include "timer.h"
-#include "usart.h"
-#include "ws2812.h"
-#include "dcf_if.h"
-#include "i2c.h"
 
 /* component includes */
 
 
-void hal_init( void )
-{
-	/* initialize the HAL subcomponents */
-	cpu_init();
-	io_init();
-	usart_init();
-	timer_init();
-	dcf_init();
-	ws2812_init();
-	i2c_init();
-}
+/** @brief initialize I2C subcomponent
+ */
+extern void i2c_init( void );
+
+#endif /* SRC_HAL_I2C_H_ */
